58e405565a8f8b02550330159b0b1773b0e7f1e8
Adi194 <>
2021-06-02 19:23:22 +0000

    added remind2()

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import sys
 from feature import pong
 from feature import reminder
 from datetime import datetime
+from asyncio import sleep
 
 
 print(pong.Pong.pong(1000))
@@ -16,6 +17,12 @@ description = 'Angry Tubby'
 
 bot = commands.Bot(command_prefix='?', description=description)
 
+'''
+async def remind(ctx):
+    channel bot.get_channel(847466369985413121)
+    await channel.send(reminder.Reminder.remind())
+'''
+
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -23,7 +30,8 @@ async def on_ready():
     print(bot.user.id)
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    if current_time >= '6:00:00' and current_time <= '12:00:00':
+    print(current_time)
+    if current_time >= '10:00:00' and current_time <= '11:00:00':
         channel = bot.get_channel(832301277119119361)
         await channel.send(reminder.Reminder.remind())
     print('------')
@@ -41,5 +49,26 @@ async def add(ctx, left: int, right: int):
 async def ping(ctx):
     await ctx.send(pong.Pong.pong(int(bot.latency)))
 
-bot.run(TOKEN)
-#bot.run("ODQ2NzE0MDMwNzM1MjI4OTY4.YKzh5A.VDYg8lQckA1TobHrwjlox-98lP"+"Y")
+@bot.command()
+async def remind2(ctx, time, *, msg):
+  unit = time[-1]
+  mul = 1
+  if unit=='s':
+    mul=1
+  if unit=='m':
+    mul = 60
+  await sleep(mul * int(time[:-1]))
+  await ctx.send(f'{msg}, {ctx.author.mention}')
+
+@bot.command()
+async def ask(ctx, msg):
+  await ctx.send(f'You will get qiqi :)')
+
+'''
+@bot.command(name='remind')
+async def remind(ctx):
+    await ctx.send(reminder.Reminder.remind())
+'''
+
+#bot.run(TOKEN)
+bot.run("ODQ2NzE0MDMwNzM1MjI4OTY4.YKzh5A.VDYg8lQckA1TobHrwjlox-98lP"+"Y")
